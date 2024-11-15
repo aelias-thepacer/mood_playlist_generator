225a9bb6ca4250345757add7c4ebf4f72cfee4bd
aelias-thepacer <aelias.thepacer@gmail.com>
2024-11-14 22:39:16 -0700

    added functionality to add song button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
 let songArraySad = [{songName:"Tourniquet", songArtist:"Zach Bryan",songInfo:"Genre: Country Released in 2023 Album: Zach Bryan" }, {songName:"Hurt", songArtist:"Nine Inch Nails", songInfo:"Genre: Industrial Rock Released in 1994 Album: The Downward Spiral"}, {songName:"Losing My Religion (Cover)", songArtist:"Shawn James",songInfo:"Genre: Folk Released in 2021 Album: The Lake Wenatchee Sessions"}, {songName:"I Hope That I Don't Fall in Love With You", songArtist:"Tom Waits",songInfo:"Genre: Folk Released in 1973 Album: Closing Time"}, {songName:"Follow You to Virgie", songArtist:"Tyler Childers",songInfo:"Genre: Country Released in 2017 Album: OurVinylSessions"}, {songName:"Achilles Come Down", songArtist:"Gang of Youths",songInfo:"Genre: Alternative Rock Released in 2017 Album: Go Farther In Lightness"}, {songName:"Atlantis", songArtist:"Seafret",songInfo:"Genre: Indie/Folk Released in 2016 Album: Tell Me Its Real"}, {songName:"Goodbye", songArtist:"Arcane",songInfo:"Genre: Soundtrack Released in 2021 Album: League Of Legends OST"}, {songName:"What Could Have Been", songArtist:"Arcane",songInfo:"Genre: Soundtrack Released in 2021 Album: League Of Legends OST"}, {songName:"I Really Want To Stay At Your House", songArtist:"Rosa Walton",songInfo:"Genre: Alternative/Indie Released in 2020 Album: Cyberpunk 2077 OST"}, {songName:"The Path (A New Beginning)", songArtist:"Gustavo Santolalla",songInfo:"Genre: Latin Released in 2013 Album: The Last Of Us OST"}, {songName:"Oh Girl", songArtist:"Eagles Of Death Of Metal",songInfo:"Genre: Rock Released in 2015 Album: Zipper Down"}, {songName:"Shes Gone Away", songArtist:"Nine Inch Nails",songInfo:"Genre: Industrial Released in 2016 Album: Not The Actual Events"}, {songName:"This Isnt The Place", songArtist:"Nine Inch Nails",songInfo:"Genre: Industrial Released in 2016 Album: Not The Acutal Events"}, {songName:"My Stress", songArtist:"NF",songInfo:"Genre: Hip Hop Released in 2019 Album: The Search"}, {songName:"Heavy", songArtist:"Linkin Park",songInfo:"Genre: Metal/Rock Released in 2017 Album: One More Light"}, {songName:"Hollow", songArtist:"Lo Spirit",songInfo:"Genre: Rock Released in 2023 Album: Hollow"}, {songName:"18002738255", songArtist:"Logic",songInfo:"Genre: Hip Hop Released in 2017 Album: Everybody"}, {songName:"All Messed Up", songArtist:"The Amity Affliction",songInfo:"Genre: Metal Released in 2016 Album: All Messed Up"}];
 let songArrayHappy = [{songName:"Why We Lose", songArtist:"Cartoon",songInfo:"Genre: Pop Released in 2015 Album: Why We Lose"},{songName:"Isle Unto Thyself", songArtist:"Miracle Music",songInfo:"Genre: Metal Released in 2012 Album: Hawaii: Part II"},{songName:"Wings (Aether 2)", songArtist:"Emile van Krieken",songInfo:"Genre: Electronic Released in 2012 Album: The Aether OST"},{songName:"Nimbostratus", songArtist:"Peppsen",songInfo:"Genre: Electronic Released in 2022 Album: SkyWorld"},{songName:"Faster Than Light", songArtist:"Paradox Interactive",songInfo:"Genre: Soundtrack Released in 2016 Album: Stellaris"},{songName:"Giza Butler", songArtist:"Sleep",songInfo:"Genre: Metal Released in 2018 Album: The Sciences"},{songName:"Hyacinth", songArtist:"Will & Floyd",songInfo:"Genre: Folk Released in 2022 Album: Will & FLoyd"},{songName:"Big in Japan", songArtist:"Tom Waits",songInfo:"Genre: Folk Released in 1999 Album: Mule Variations"},{songName:"Hero Of The Day", songArtist:"Metallica",songInfo:"Genre: Hard rock Released in 1996 Album: Load"},{songName:"Batshit", songArtist:"Lights",songInfo:"Genre: Electronic Released in 2020 Album: Dead End"},{songName:"Sunroof", songArtist:"Nicky Youre & Day",songInfo:"Genre: Indie/Pop Released in 2022 Album: Sunroof"},{songName:"Not the 1975", songArtist:"Knox",songInfo:"Genre: Pop Released in 2023 Album: Im So Good At Being Home Alone?"},{songName:"Sh-Boom", songArtist:"The Crew Cuts",songInfo:"Genre: Pop Released in 1996 Album: The Best Of The Crew Cuts"},{songName:"Way of the Triune God", songArtist:"Tyler Childers",songInfo:"Genre: Country Released in 2022 Album: Can I Take My Hounds To Heaven?"},{songName:"Ocean Man", songArtist:"Ween",songInfo:"Genre: Alternative/Indie Released in 1997 Album: The Mollusk"},{songName:"Bite Hard", songArtist:"Franz Ferdinand",songInfo:"Genre: Alternative/Indie Released in 2009 Album: Tonight"},{songName:"Night Life", songArtist:"Mrs. Magician",songInfo:"Genre: Alternative/Indie Released in 2012 Album: Night Life"}];
-let songArrayLove = [{songName:"Make It Wit Chu", songArtist:"Queens of The Stone Age",songInfo:"Genre: Rock Released in 2007 Album: Era Vulgaris"}, {songName: "In Your Arms" , songArtist: "Hellogoodbye",songInfo:"Genre: pop/rock Released in 2006 Album: Zombies!Aliens!Vampires!Dinosaurs!"},{songName: "Sleazy Bed Track" , songArtist: "The Bluetones",songInfo:"Genre Pyschedelic Rock Released in 1998 Album: Return To The Last Chance Saloon"},{songName: "Pay No Rent" , songArtist: "Turnpike Troubadours",songInfo:"Genre: Country Released in 2017 Album: A Long Way From Your Heart"},{songName: "You Got Me" , songArtist: "The Roots",songInfo:"Genre: Hip-Hop Released in 1999 Album: Things Fall Apart"},{songName: "We Are The Night" , songArtist: "Madison Mars",songInfo:"Genre: Dance/Electronic Released in 2017 Album: We Are The Night"},{songName: "Kiss From A Rose" , songArtist: "Wake Me",songInfo:"Genre: Hard rock Released in 2017 Album: Kiss From A Rose"},{songName: "Come 2 Me" , songArtist: "Johnny Goth",songInfo:"Genre:Alternative/Indie Released in 2022 Album: Voulez-Vous"},{songName: "Too Sweet" , songArtist: "Hozier",songInfo:"Genre: Alternative/Indie Released in 2024 Album: Unheard"},{songName: "Ride It" , songArtist: "Regard",songInfo:"Genre: Dance/Electronic Released in 2019 Album: Ride It"},{songName: "Sun To Me" , songArtist: "Machine Gun Kelly",songInfo:"Genre: Rock Released in 2024 Album: sun to me"},{songName: "Life Was Easier When I Only Cared About Me" , songArtist: "Bad Suns",songInfo:"Genre: Alternative/Indie Released in 2022 Album: Apocalypse Whenever"},{songName:"DYWTYLM", songArtist:"Sleep Token",songInfo:"Genre: Rock Released in 2023 Album: Take Me Back To Eden"}];
+let songArrayLove = [{songName:"Make It Wit Chu", songArtist:"Queens of The Stone Age", songInfo:"Genre: Rock Released in 2007 Album: Era Vulgaris"}, {songName: "In Your Arms" , songArtist: "Hellogoodbye",songInfo:"Genre: pop/rock Released in 2006 Album: Zombies!Aliens!Vampires!Dinosaurs!"},{songName: "Sleazy Bed Track" , songArtist: "The Bluetones",songInfo:"Genre Pyschedelic Rock Released in 1998 Album: Return To The Last Chance Saloon"},{songName: "Pay No Rent" , songArtist: "Turnpike Troubadours",songInfo:"Genre: Country Released in 2017 Album: A Long Way From Your Heart"},{songName: "You Got Me" , songArtist: "The Roots",songInfo:"Genre: Hip-Hop Released in 1999 Album: Things Fall Apart"},{songName: "We Are The Night" , songArtist: "Madison Mars",songInfo:"Genre: Dance/Electronic Released in 2017 Album: We Are The Night"},{songName: "Kiss From A Rose" , songArtist: "Wake Me",songInfo:"Genre: Hard rock Released in 2017 Album: Kiss From A Rose"},{songName: "Come 2 Me" , songArtist: "Johnny Goth",songInfo:"Genre:Alternative/Indie Released in 2022 Album: Voulez-Vous"},{songName: "Too Sweet" , songArtist: "Hozier",songInfo:"Genre: Alternative/Indie Released in 2024 Album: Unheard"},{songName: "Ride It" , songArtist: "Regard",songInfo:"Genre: Dance/Electronic Released in 2019 Album: Ride It"},{songName: "Sun To Me" , songArtist: "Machine Gun Kelly",songInfo:"Genre: Rock Released in 2024 Album: sun to me"},{songName: "Life Was Easier When I Only Cared About Me" , songArtist: "Bad Suns",songInfo:"Genre: Alternative/Indie Released in 2022 Album: Apocalypse Whenever"},{songName:"DYWTYLM", songArtist:"Sleep Token",songInfo:"Genre: Rock Released in 2023 Album: Take Me Back To Eden"}];
 let songArrayAngry = [{songName:"Rats", songArtist:"Cancer Bats",songInfo:"Genre: Groove metal Released in 2012 Album: Dead Set On Living"}, {songName:"My Heart is the Worst Kind of Weapon", songArtist:"Fall Out Boy",songInfo:"Genre: pop-funk Released in 2006 Album: From Under The Cork Tree"}, {songName:"Track 1 R", songArtist:"'68",songInfo:"Genre: Rock Released in 2014 Album: In Humor And Sadness"},{songName:"Mother", songArtist:"IDLES",songInfo:"Genre: Punk-Rock Released in 2017 Album: Brutalism"},{songName:"Troglodyte", songArtist:"Viagra Boys",songInfo:"Genre: Post-Punk Released in 2022 Album: Cave World"}, {songName:"Blood And Thunder", songArtist:"Mastodon",songInfo:"Genre: Metal Released in 2004 Album: Leviathan"}, {songName:"Cyberpsychosis", songArtist:"Faderhead",songInfo:"Genre: Dance/Electronic Released in 2020 Album: 2077"}, {songName:"God Run", songArtist:"505 Games",songInfo:"Genre: Soundtrack Released in 2023 Album: Ghostrunner 2 OST"}, {songName:"Blood Code", songArtist:"Le Castle Vania",songInfo:"Genre: Electronic Released in 2023 Album: John Wick 4 OST"}, {songName:"Half-Life", songArtist:"Essenger",songInfo:"Genre: Dance/Electronic Released in 2020 Album: After Dark"}, {songName:"Purify", songArtist:"Metallica",songInfo:"Genre: Metal Released in 2003 Album: St.Anger"}, {songName:"Freebooter", songArtist:"High On Fire",songInfo:"Genre: Metal Released in 2018 Album: Electric Messiah"}, {songName:"One Gun", songArtist:"Lamb Of God",songInfo:"Genre: Metal Released in 2004 Album: Ashes Of The Wake"}, {songName:"Explosia", songArtist:"Gojira",songInfo:"Genre: Metal Released in 2012 Album: L'Enfant Sauvage"}, {songName:"Why You Gotta Kick Me While I’m Down", songArtist:"Bring Me The Horizon",songInfo:"Genre: Metal Released in 2019 Album: amo"}, {songName:"Deep End", songArtist:" I Prevail",songInfo:"Genre: Metalcore Released in 2022 Album: TRUE POWER"}, {songName:"Reinventing Your Exit", songArtist:"Underoath",songInfo:"Genre: Rock Released in 2004 Album: They're Only Chasing Safety"}, {songName:"The Crown", songArtist:"Stick To Your Guns",songInfo:"Genre: Hardcore Punk Released in 2015 Album: Disobedient"}];
 let songArrayCalm = [{songName:"Blue Reverie", songArtist:"ERRA",songInfo:"Genre: Metal Released in 2024 Album: CURE"},{songName:" We Out Of Sight", songArtist:"Hundredth",songInfo:"Genre: Alt/Indie Released in 2020 Album: Somewhere Nowhere"},{songName:"Lesser Gods", songArtist:"Ghost Atlas",songInfo:"Genre: Rock Released in 2024 Album: Dust Of The Human Shape"},{songName:"Slow Motion", songArtist:"Flor",songInfo:"Genre: Dance/Electronic Released in 2019 Album: ley lines"},{songName:"Fluff", songArtist:"Black Sabbath",songInfo:"Genre: Metal Released in 1973 Album: Sabbath Bloody Sabbath"},{songName:"The Golden Rose", songArtist:"Tom Petty",songName:"Genre: Rock Released in 2006 Album: Highway Companion"},{songName:"Intermission", songArtist:"Truckerfighters",songInfo:"Genre: Rock Released in 2005 Album: Gravity X"},{songName:"Kana Kassy", songArtist:"Ali Farke Toure",songInfo:"Genre: Desert Blues Released in 2010 Album: Ali & Toumani"},{songName:"Sweden", songArtist:"C418",songInfo:"Genre: Soundtrack Released in 2011 Album: Minecraft OST"},{songName:"The River", songArtist:"Andrew Prahlow",songInfo:"Genre: Dance/Electronic Released in 2021 Album: Outer Wilds: Echoes of the Eye OST"},{songName:"Robotic Assist Module", songArtist:"Wojciech Golczewskie",songInfo:"Genre: Dance/Electronic Released in 2017 Album: The Signal"},{songName:"Aria Math", songArtist:"C418",songInfo:"Genre: Soundtrack Released in 2013 Album: Minecraft OST"},{songName:"Lost Ember Main Title", songArtist:"Will Morton",songInfo:"Genre: Soundtrack Released in 2019 Album: Lost Ember OST"},{songName:"Winter Is All Over You (Baauer Remix)", songArtist:"First Aid Kit",songInfo:"Genre: Folk Released in 2010 Album: The Big Black and the Blue"},{songName:"Alrighty Aphrodite", songArtist:"Peach Pit",songInfo:"Genre: Alt/Indie Released in 2018 Album: Being So Normal"},{songName:"We've Never Met but Can We Have a Cup of Coffee or Something", songArtist:"In Love With A Ghost",songInfo:"Genre: Dance/Electronic Released in 2017 Album: Let's Go"},{songName:"Hurt", songArtist:"Yung Lean",songInfo:"Genre: Hip-Hop Released in 2013 Album: Unknown Death 2002"},{songName:"November Air", songArtist:"Zach Bryan",songInfo:"Genre: Country Released in 2020 Album: Quiet, Heavy Dreams"}];
 
+// Sets up local storage if it does not already exist
+if (localStorage.getItem('songArraySad') == null) {
+    localStorage.setItem('songArraySad', JSON.stringify(songArraySad));
+}
+if (localStorage.getItem('songArrayHappy') == null) {
+    localStorage.setItem('songArrayHappy', JSON.stringify(songArrayHappy));
+}
+if (localStorage.getItem('songArrayLove') == null) {
+    localStorage.setItem('songArrayLove', JSON.stringify(songArrayLove));
+}
+if (localStorage.getItem('songArrayAngry') == null) {
+    localStorage.setItem('songArrayAngry', JSON.stringify(songArrayAngry));
+}
+if (localStorage.getItem('songArrayCalm') == null) {
+    localStorage.setItem('songArrayCalm', JSON.stringify(songArrayCalm));
+}
+
 // Declares global variables.
-let currentMood = songArraySad;
+let currentMood = JSON.parse(localStorage.getItem("songArraySad"));
+let currentMoodString = "songArraySad";
 let numberOfSongs = 10;
 let shuffleArray = [0,1,2,3,4,5,6,7,8,9];
 let songInfoArray = [];
@@ -14,9 +32,14 @@ let songInfoArray = [];
 let songsDivEl = document.querySelector('#songsDiv');
 let songsInfoEl = document.querySelector('#infoDiv')
 var moodModal = new bootstrap.Modal(document.getElementById('moodModal'));
+var formModal = new bootstrap.Modal(document.getElementById('formModal'));
 
 // Selects [songsCalled] number of songs from [songArray] and calls objectMaker on each.
 function songSelector(songArray, songsCalled) {
+    shuffleArray = [];
+    for (let index = 0; index < songArray.length; index++) {
+        shuffleArray.push(index);
+    }
     songsDivEl.innerHTML = "";
     songInfoArray = [];
     let song;
@@ -69,15 +92,20 @@ function objectMaker(titleLarge, titleSmall, appendEl) {
 // Changes currentMood based on string input and dismisses mood buttons
 function moodSelector(mood) {
     if (mood == "happy") {
-        currentMood = songArrayHappy;
+        currentMood = JSON.parse(localStorage.getItem("songArrayHappy"));;
+        currentMoodString = "songArrayHappy";
     } else if (mood == "sad") {
-        currentMood = songArraySad;
+        currentMood = JSON.parse(localStorage.getItem("songArraySad"));;
+        currentMoodString = "songArraySad";
     } else if (mood == "angry") {
-        currentMood = songArrayAngry;
+        currentMood = JSON.parse(localStorage.getItem("songArrayAngry"));;
+        currentMoodString = "songArrayAngry";
     } else if (mood == "love") {
-        currentMood = songArrayLove;
+        currentMood = JSON.parse(localStorage.getItem("songArrayLove"));;
+        currentMoodString = "songArrayLove";
     } else if (mood == "calm") {
-        currentMood = songArrayCalm;
+        currentMood = JSON.parse(localStorage.getItem("songArrayCalm"));;
+        currentMoodString = "songArrayCalm";
     }
 
     songSelector(currentMood, numberOfSongs);
@@ -96,13 +124,37 @@ function songInformationDisplay() {
     }
 }
 
-// Sets up object listeners for buttons.
+// Shows formModal when someone wants to add a song
+function addSong() {
+    formModal.show();
+}
+
+// submits song information if the song modal is filled out
+function submitSongInfo(event) {
+    event.preventDefault();
+    if ((document.querySelector('#formTitle').value && document.querySelector('#formArtist').value && document.querySelector('#formGenre').value && document.querySelector('#formAlbum').value) != "") {
+        let newSong = {songName:document.querySelector('#formTitle').value , songArtist:document.querySelector('#formArtist').value , songInfo:("Genre: " + document.querySelector('#formGenre').value + " Album: " + document.querySelector('#formAlbum').value)};
+        storeSong(newSong);
+        currentMood = JSON.parse(localStorage.getItem(currentMoodString));
+        formModal.hide();
+    }
+}
+
+// stores song information in local storage
+function storeSong(object) {
+    let songDataArray = JSON.parse(localStorage.getItem(currentMoodString));
+    songDataArray.push(object);
+    localStorage.setItem(currentMoodString, JSON.stringify(songDataArray));
+}
+
+// Sets up object listeners.
 document.getElementById("rerollButton").addEventListener("click", () => songSelector(currentMood, numberOfSongs));
 document.getElementById("addSongButton").addEventListener("click", addSong);
 document.getElementById("sadButton").addEventListener("click", () => moodSelector("sad"));
 document.getElementById("angryButton").addEventListener("click", () => moodSelector("angry"));
 document.getElementById("happyButton").addEventListener("click", () => moodSelector("happy"));
 document.getElementById("loveButton").addEventListener("click", () => moodSelector("love"));
+document.getElementById("submit").addEventListener("click", submitSongInfo);
 
 
 moodModal.show();
